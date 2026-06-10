7da9593ea470e409bac50269e541c02cbd617b0d
Pedro Xavier <phmxavier@gmail.com>
2026-06-10 14:48:39 -0300

    refactor: update deployment workflow and configuration files for improved build process

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter  basename={import.meta.env.PUBLIC_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
